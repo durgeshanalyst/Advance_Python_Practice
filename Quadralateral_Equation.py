1feac618264bc411ca2c15ff65f0d6df720066f0
Durgesh Kumar <durgeshkumar7b@gmail.com>
2023-09-20 19:24:19 +0530

    Create Quadralateral_Equation.py

---
+++ Quadralateral_Equation.py
@@ -0,0 +1,13 @@
+# Write python function to print X and Y roots of A,B and C
+# Reference : Q
+#Calculate the discriminant D = b^2 - 4ac.
+# Compute the two solutions for x and y using the quadratic formula:
+# x = (-b + √D) / (2a)
+# y = (-b - √D) / (2a)
+# Print the values of x and y. 
+
+def find_roots(entries):
+    arr= list(map(int(input().split())))
+    print(arr)
+
+find_roots(list(map(int(input().split()))))      
